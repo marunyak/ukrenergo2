141b9f0421a9e3b92b061054a47e2800ed92ca09
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-18 14:56:06 +0000

    Deploy marunyak/ukrenergo2 to github.com/marunyak/ukrenergo2.git:gh-pages

---
+++ precache-manifest.e5522d47dd0b5f8bdefee3a1441dc2d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edce93d444585e1d74ea0efdccf6fa63",
+    "url": "/ukrenergo2/index.html"
+  },
+  {
+    "revision": "c27a44828eb911d2c978",
+    "url": "/ukrenergo2/static/css/main.a442520c.chunk.css"
+  },
+  {
+    "revision": "1021e7287fc53b7276fc",
+    "url": "/ukrenergo2/static/js/2.d39cde54.chunk.js"
+  },
+  {
+    "revision": "c27a44828eb911d2c978",
+    "url": "/ukrenergo2/static/js/main.7a022960.chunk.js"
+  },
+  {
+    "revision": "90e87955f2e99d86963e",
+    "url": "/ukrenergo2/static/js/runtime-main.88ea400f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ukrenergo2/precache-manifest.e5522d47dd0b5f8bdefee3a1441dc2d2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ukrenergo2/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
